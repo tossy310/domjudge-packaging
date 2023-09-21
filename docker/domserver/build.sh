8a00483fb9ccc81a2673c0bae4a603c8a48ee44b
Hiroki Sato <hsugar.310@gmail.com>
2023-09-21 23:59:59 +0900

    Use profile and login shell to activate venv

--- docker/domserver/build.sh
+++ docker/domserver/build.sh
@@ -3,7 +3,7 @@
 cd /domjudge-src/domjudge*
 chown -R domjudge: .
 # If we used a local source tarball, it might not have been built yet
-sudo -u domjudge sh -c '. /venv/bin/activate && make dist'
+sudo -u domjudge -l make dist
 sudo -u domjudge ./configure -with-baseurl=http://localhost/
 
 # Passwords should not be included in the built image. We create empty files here to prevent passwords from being generated.
@@ -25,8 +25,5 @@ then
 	rm /opt/domjudge/domserver/webapp/.env.local /opt/domjudge/domserver/webapp/.env.local.php
 fi
 
-sudo -u domjudge sh -c '. /venv/bin/activate && make docs'
-# Use Python venv to use the latest Sphinx to build DOMjudge docs.
-# shellcheck source=/dev/null
-. /venv/bin/activate
+sudo -u domjudge -l make docs
 make install-docs
